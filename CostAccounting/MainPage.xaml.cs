ac2d7a0ef3f358a6345b56f2401dc0c99c5fdb5c
Даниил <dragon0lordchik@gmail.com>
2022-03-14 12:38:37 +0300

    Передаю эстафету Искандеру

--- CostAccounting/MainPage.xaml.cs
+++ CostAccounting/MainPage.xaml.cs
@@ -1,18 +1,31 @@
-﻿using System;
+﻿using Microcharts;
+using System;
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using Xamarin.Forms;
+using Entry = Microcharts.Entry;
 
 namespace CostAccounting
 {
     public partial class MainPage : ContentPage
     {
+        List<Entry> entries = new List<Entry>
+        {
+            new Entry()
+            {
+                Color = Color.White,
+                Label = "January",
+                ValueLabel = "200"
+            }
+        };
         public MainPage()
         {
             InitializeComponent();
+
+            Chart1.Chart = new DonutChart { Entries = entries };
         }
     }
 }
