804ff1cba8dab04dcc61765d08f2c1c70617a4de
Даниил <dragon0lordchik@gmail.com>
2022-03-17 14:46:59 +0300

    Bd is done

---
+++ CostAccounting/LoacalRepository.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections.Generic;
+using SQLite;
+
+namespace CostAccounting
+{
+    public class LocalRepository
+    {
+        SQLiteConnection database;
+        public LocalRepository(string databasePath)
+        {
+            database = new SQLiteConnection(databasePath);
+            database.CreateTable<Item>();
+        }
+        public IEnumerable<Item> GetItems()
+        {
+            return database.Table<Item>().ToList();
+        }
+        public Item GetItem(int id)
+        {
+            return database.Get<Item>(id);
+        }
+        public int DeleteItem(int id)
+        {
+            return database.Delete<Item>(id);
+        }
+        public int SaveItem(Item item)
+        {
+            if (item.Id != 0)
+            {
+                database.Update(item);
+                return item.Id;
+            }
+            else
+            {
+                return database.Insert(item);
+            }
+        }
+    }
+}
