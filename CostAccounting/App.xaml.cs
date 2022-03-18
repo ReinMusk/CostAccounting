35b951764d21bd122a09495b50269982e64514e2
Искандер Шарафеев <iskin20003@gmail.com>
2022-03-18 13:17:27 +0300

    Demo version

--- CostAccounting/App.xaml.cs
+++ CostAccounting/App.xaml.cs
@@ -1,16 +1,33 @@
 ﻿using System;
+using System.IO;
 using Xamarin.Forms;
 using Xamarin.Forms.Xaml;
+
 
 namespace CostAccounting
 {
     public partial class App : Application
     {
+        public const string DATABASE_NAME = "localCost.db";
+        public static LocalRepository database;
+        public static LocalRepository Database
+        {
+            get
+            {
+                if (database == null)
+                {
+                    database = new LocalRepository(
+                    Path.Combine(
+                    Environment.GetFolderPath(Environment.SpecialFolder.LocalApplicationData), DATABASE_NAME));
+                }
+                return database;
+            }
+        }
         public App()
         {
             InitializeComponent();
 
-            MainPage = new MainPage();
+            MainPage = new NavigationPage(new MainPage());
         }
 
         protected override void OnStart()
--- CostAccounting/MainPage.xaml.cs
+++ CostAccounting/MainPage.xaml.cs
@@ -13,20 +13,41 @@ namespace CostAccounting
 {
     public partial class MainPage : ContentPage
     {
-        List<ChartEntry> entries = new List<ChartEntry>
-        {
-            new ChartEntry(200)
-            {
-                Color = SKColor.Parse("#FF1493"),
-                Label = "January",
-                ValueLabel = "200"
-            }
-        };
+        DatePicker datePicker; 
+
+        
         public MainPage()
         {
             InitializeComponent();
 
+            datePicker = new DatePicker
+            {
+                Format = "D",
+                MaximumDate = DateTime.Now.AddDays(5),
+                MinimumDate = DateTime.Now.AddDays(-5)
+            };
+
+            datePicker.DateSelected += datePicker_DateSelected;
+
+
+            List<ChartEntry> entries = new List<ChartEntry>
+            {
+                new ChartEntry(200)
+                {
+                    Color = SKColor.Parse("#FF1493")
+                },
+                new ChartEntry(300)
+                {
+                    Color = SKColor.Parse("#FF2222")
+                },
+            };
+
             Chart1.Chart = new DonutChart { Entries = entries };
+        }
+
+        private void datePicker_DateSelected(object sender, DateChangedEventArgs e)
+        {
+            
         }
     }
 }
